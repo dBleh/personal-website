daa410861d63b257c27f7b28652a73c13aed2f1f
Duncan <duncanblais@gmail.com>
2025-04-07 14:53:51 -0700

    Moved mobile check to layout for geobuild

--- src/app/geobuild/layout.tsx
+++ src/app/geobuild/layout.tsx
@@ -1,13 +1,14 @@
 'use client';
-
+import useIsMobile from '../../hooks/usIsMobile';
 export default function GeoBuildLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const isMobile = useIsMobile()
   return (
     <div className="w-full h-full">
-      {children}
+      {isMobile?"Geobuild is not supported on mobile":children}
     </div>
   );
 }
--- src/app/geobuild/page.tsx
+++ src/app/geobuild/page.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import GeoBuild from './GeoBuild';
-import useIsMobile from '../../hooks/usIsMobile';
+
 export const metadata = {
   title: 'GeoBuild - 3D Building Tool',
   description: 'Create 3D structures with this interactive builder',
 };
 
 export default function GeoBuildPage() {
-  const isMobile = useIsMobile()
   return (
     <main>
-    {isMobile? <GeoBuild />:"GeoBuild is not supported on mobile devices"}
-      
+      <GeoBuild />
     </main>
   );
 }
